61605c390b1d6e24ddcfe15382f79959690d30fc
Jonathan Cheng <jcheng461@gmail.com>
2022-09-09 18:35:59 +0900

    WIP: add method in TicketControl for handling editing ticket

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -54,12 +54,21 @@ class TicketControl extends React.Component {
     this.setState({ editing: true });
   };
 
+  handleEditTicketInList = (ticketToEdit) => {
+    const editedMainTicketList = this.state.mainTicketList.filter((ticket) => ticket.id !== this.state.selectedTicket.id).concat(ticketToEdit);
+    this.setState({
+      mainTicketList: editedMainTicketList,
+      editing: false,
+      selectedTicket: null,
+    });
+  };
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
 
     if (this.state.editing) {
-      currentlyVisibleState = <EditTicketForm ticket={this.state.selectedTicket} />;
+      currentlyVisibleState = <EditTicketForm ticket={this.state.selectedTicket} onEditTicket={this.handleEditingTicketInList} />;
       buttonText = "Return to Ticket List";
     } else if (this.state.selectedTicket != null) {
       currentlyVisibleState = (
