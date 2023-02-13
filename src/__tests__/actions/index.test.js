0a176e7adc052abc5d469ae1fa7ffbce8a77c3a3
Jonathan Cheng <jcheng461@gmail.com>
2023-02-13 22:23:34 +0900

    refactor code to add action creator for DELETE_TICKET action. Action creator passes 1st test

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,11 @@
+// Tests for actions
+import * as actions from "./../../actions";
+
+describe("Help Queue actions", () => {
+  it("deleteTicket should create DELETE_TICKET action", () => {
+    expect(actions.deleteTicket(1)).toEqual({
+      type: "DELETE_TICKET",
+      id: 1,
+    });
+  });
+});
