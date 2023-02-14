8ae9655d7ea005d950f205001428d1042c71e3c7
Jonathan Cheng <jcheng461@gmail.com>
2023-02-14 23:36:44 +0900

    add action creators for TOGGLE_FORM and ADD_TICKET. Both pass tests

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -8,4 +8,25 @@ describe("Help Queue actions", () => {
       id: 1,
     });
   });
+  it("toggleForm should create TOGGLE_FORM action", () => {
+    expect(actions.toggleForm()).toEqual({
+      type: "TOGGLE_FORM",
+    });
+  });
+  it("addTicket should create ADD_TICKET action", () => {
+    expect(
+      actions.addTicket({
+        names: "Homer and Bart",
+        location: "Moe's Tavern",
+        issue: "out of Duff Beer",
+        id: 1,
+      })
+    ).toEqual({
+      type: "ADD_TICKET",
+      names: "Homer and Bart",
+      location: "Moe's Tavern",
+      issue: "out of Duff Beer",
+      id: 1,
+    });
+  });
 });
